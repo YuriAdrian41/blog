ebf58d1b348bde87569f0b2f0d66955b0e9441ef
YuriAdrian41 <donaldlafranchi@gmail.com>
2022-10-13 03:08:40 +0900

    2.1 Creating a Standalone Migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_12_173500) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_12_180705) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -29,12 +29,25 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_12_173500) do
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
+  create_table "customers", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "customers_products", id: false, force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.integer "product_id", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
     t.string "price"
+    t.string "part_number"
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
   create_table "publications", force: :cascade do |t|
@@ -70,4 +83,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_12_173500) do
   end
 
   add_foreign_key "comments", "articles"
+  add_foreign_key "products", "users"
 end
