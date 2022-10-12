6d60b2045cb6f1648fcc6172f1e0095d55c4a6f3
YuriAdrian41 <donaldlafranchi@gmail.com>
2022-10-13 03:43:38 +0900

    3.1 Creating a Table

--- db/migrate/20221012111147_create_products.rb
+++ db/migrate/20221012111147_create_products.rb
@@ -1,7 +1,7 @@
 class CreateProducts < ActiveRecord::Migration[7.0]
   def change
-    create_table :products do |t|
-      t.string :name
+    create_table :products, options: "ENGINE=BLACKHOLE" do |t|
+      t.string :name, null: false
 
       t.timestamps
     end
