bea4206e935f0776503644a4b616ac0865d5ddd9
YuriAdrian41 <donaldlafranchi@gmail.com>
2022-10-13 03:14:20 +0900

    2.2 Model Generators

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,8 +1,6 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  set_fixture_class my_projects: Product
-  fixtures :my_projects
   # test "the truth" do
   #   assert true
   # end
