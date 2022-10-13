a70c828e24e20dec5025ff3cb1313de7291c714e
YuriAdrian41 <donaldlafranchi@gmail.com>
2022-10-14 04:08:27 +0900

    2.4 comparison

---
+++ db/migrate/20221013190357_create_promotions.rb
@@ -0,0 +1,10 @@
+class CreatePromotions < ActiveRecord::Migration[7.0]
+  def change
+    create_table :promotions do |t|
+      t.datetime :start_date
+      t.datetime :end_date
+
+      t.timestamps
+    end
+  end
+end
