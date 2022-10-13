1ffde66c5550ac7e89af260467ee826f0ffe88b6
YuriAdrian41 <donaldlafranchi@gmail.com>
2022-10-13 21:47:52 +0900

    6 Schema Dumping and You

--- db/schema.rb
+++ db/schema.rb
@@ -10,93 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_13_092023) do
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "status"
-  end
-
-  create_table "categories", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "categories_products", id: false, force: :cascade do |t|
-    t.integer "product_id", null: false
-    t.integer "category_id", null: false
-    t.index ["category_id"], name: "index_categories_products_on_category_id"
-    t.index ["product_id"], name: "index_categories_products_on_product_id"
-  end
-
-  create_table "comments", force: :cascade do |t|
-    t.string "commenter"
-    t.text "body"
-    t.integer "article_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "status"
-    t.index ["article_id"], name: "index_comments_on_article_id"
-  end
-
-  create_table "customers", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "customers_products", id: false, force: :cascade do |t|
-    t.integer "customer_id", null: false
-    t.integer "product_id", null: false
-  end
-
-  create_table "products", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "price"
-    t.string "part_number"
-    t.string "supplier_type", null: false
-    t.integer "supplier_id", null: false
-    t.string "upc_code"
+ActiveRecord::Schema[7.0].define(version: 2022_09_06_171750) do
+  create_table "authors", force: true do |t|
     t.string "name"
-    t.integer "user_id"
-    t.index ["part_number"], name: "index_products_on_part_number"
-    t.index ["supplier_type", "supplier_id"], name: "index_products_on_supplier"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "publications", force: :cascade do |t|
-    t.string "title"
+  create_table "products", force: true do |t|
+    t.string "name"
     t.text "description"
-    t.integer "publication_type_id"
-    t.string "publisher_type"
-    t.integer "publisher_id"
-    t.boolean "single_issue"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["publication_type_id"], name: "index_publications_on_publication_type_id"
-    t.index ["publisher_type", "publisher_id"], name: "index_publications_on_publisher"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string "part_number"
   end
-
-  create_table "publishers", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "pulication_types", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "occupation"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "max_login_attempts"
-    t.boolean "must_change_password"
-  end
-
-  add_foreign_key "comments", "articles"
-  add_foreign_key "products", "users"
 end
