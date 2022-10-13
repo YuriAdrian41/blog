3643ffd9e5ce6a3432172d761423ce83a451f14a
YuriAdrian41 <donaldlafranchi@gmail.com>
2022-10-13 21:11:33 +0900

    3.11 Using the up/down Methods

--- db/migrate/20221013100032_example_migration.rb
+++ db/migrate/20221013100032_example_migration.rb
@@ -1,27 +1,29 @@
 class ExampleMigration < ActiveRecord::Migration[7.0]
-  def change
+  def up
     create_table :distributors do |t|
       t.string :zipcode
     end
 
-    reversible do |dir|
-      dir.up do
-        # add a CHECK constraint
-        execute <<-SQL
-          ALTER TABLE distributors
-            ADD CONSTRAINT zipchk
-              CHECK (char_length(zipcode) = 5) NO INHERIT;
-        SQL
-      end
-      dir.down do
-        execute <<-SQL
-          ALTER TABLE distributors
-            DROP CONSTRAINT zipchk
-        SQL
-      end
-    end
+    # add a CHECK constraint
+    execute <<-SQL
+      ALTER TABLE distributors
+        ADD CONSTRAINT zipchk
+        CHECK (char_length(zipcode) = 5) NO INHERIT;
+    SQL
 
     add_column :users, :home_page_url, :string
     rename_column :users, :email, :email_address
+  end
+
+  def down
+    rename_column :users, :email_address, :email
+    remove_column :users, :home_page_url
+
+    execute <<-SQL
+      ALTER TABLE distributors
+        DROP CONSTRAINT zipchk
+    SQL
+
+    drop_table :distributors
   end
 end
