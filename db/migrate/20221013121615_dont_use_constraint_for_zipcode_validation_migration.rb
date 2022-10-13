4af525eaa7d192b5107754ff248f33de649f7bf4
YuriAdrian41 <donaldlafranchi@gmail.com>
2022-10-13 21:19:13 +0900

    3.12 Reverting Previous Migrations

---
+++ db/migrate/20221013121615_dont_use_constraint_for_zipcode_validation_migration.rb
@@ -0,0 +1,25 @@
+class DontUseConstraintForZipcodeValidationMigration < ActiveRecord::Migration[7.0]
+  def change
+    revert do
+      # copy-pasted code from ExampleMigration
+      reversible do |dir|
+        dir.up do
+          # add a CHECK constraint
+          execute <<-SQL
+            ALTER TABLE distributors
+              ADD CONSTRAINT zipchk
+                CHECK (char_length(zipcode) = 5);
+          SQL
+        end
+        dir.down do
+          execute <<-SQL
+            ALTER TABLE distributors
+              DROP CONSTRAINT zipchk
+          SQL
+        end
+      end
+
+      # The rest of the migration was ok
+    end
+  end
+end
